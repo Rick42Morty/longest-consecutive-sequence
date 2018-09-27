48e8f9c26768bf2d7011c34d4124652bf0ea1adc
Rick42Morty <evgenevgen00@mail.ru>
2018-09-28 01:00:16 +0300

    add final solution

--- src/index.js
+++ src/index.js
@@ -2,73 +2,30 @@ module.exports = function longestConsecutiveLength(array) {
   // your solution here
   if (array.length == 0) return 0;
   if (array.length == 1) return 1;
-
-  var length = array.length;
-  var min = array[0];
-  var max = min;
-
-  var con = 1;
-  var conMax = 1;
-  var diff = 0;
-
-  var sorted = 1;
-
-  for (var i = 0; i < length - 1; i++) {
-    if (array[i+1] < array[i]) {
-      sorted = 0;
-      break;
-    }
-  }
-
-  if (sorted) {
-    for (var i = 0; i < length - 1; i++) {
-      diff = array[i+1] - array[i];
-      if (diff == 0 || diff == 1) {
-        con += diff;
-      } else {
-        if (conMax < con) {
-          conMax = con;
-        }
-        con = 1;
-      }
-    }
-
-    return conMax;
-  }
-
-  for (var i=0; i < length; i++) {
-    if (array[i] < min) min = array[i];
-    if (array[i] > max) max = array[i];
-  }
-
-  for (var i=0; i < length; i++) {
-    array[i] -= min;
-  }
-
+  
+  let arr = array.slice();
+  const length = arr.length;
+  let con = 1;
+  let conMax = 1;
+ 
   //формируем вспомогательный массив
-  var help = [];
+  let help = [];
 
-  for (var i = 0; i < max - min; i++) {
-    help[i] = 0;
-  }
-
-  for (i=0; i < length; i++) {
-    help[array[i]] = 1;
+  for (let i=0; i < length; i++) {
+    help[arr[i]] = 1;
   }
 
   //находим самую длинную последовательность единичек в help
-
-
-  for (i = 0; i < max - min - 1; i++) {
-    if (help[i] == help[i+1] && help[i] == 1) {
+  //используем тот факт, что массив - тоже объект
+  
+  for (key in help) {
+    if (help[key] == help[key-1]) {
       con++;
       continue;
     }
     if (con > conMax) conMax = con;
     con = 1;
   }
-
-  if (con > conMax) conMax = con;
 
   return conMax;
 }
